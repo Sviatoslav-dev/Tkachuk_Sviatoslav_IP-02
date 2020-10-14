2ffc8648688470be8f764937bb1b5b714d9ee46e
Sviatoslav-dev <70691739+Sviatoslav-dev@users.noreply.github.com>
2020-10-14 14:15:00 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,3 +1,6 @@
+// 33 варіант
+// завдання: Задано сторону квадрата. Знайти його діагональ, периметр та площу.
+
 #include<iostream>
 #include<math.h>
 
@@ -5,25 +8,14 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-  float a, b, c, alpha, beta, gamma, PI = 3.14159265;
+  float a;
 
-  cout <<"enter a"<< '\n';
-  cin >> a;
-  cout <<"enter b"<< '\n';
-  cin >> b;
-  cout <<"enter c"<< '\n';
-  cin >> c;
+  cout << "a = ";
+  cin>> a;
 
-  alpha = acos((c * c + b * b - a * a)/(2 * b * c));
-  beta = acos((c * c + a * a - b * b)/(2 * a * c));
-  gamma = acos((b * b + a * a - c * c)/(2 * a * b));
-
-  cout <<"in degrees:"<< '\n';
-  cout <<(alpha * 180) / PI<<' '<<(beta * 180) / PI<<' '<<(gamma * 180) / PI<< '\n';
-
-  cout <<"in radians:"<< '\n';
-  cout <<alpha<<' '<<beta<<' '<<gamma<< '\n';
-
+  cout << "d = " << a * sqrt(2) << '\n';
+  cout << "P = " << a * 4 << '\n';
+  cout << "S = " << a * a << '\n';
 
   return 0;
 }
