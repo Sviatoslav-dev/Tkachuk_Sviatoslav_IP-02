a7d09905c33bee8f159614249ec387b5174cf4af
Sviatoslav-dev <70691739+Sviatoslav-dev@users.noreply.github.com>
2020-10-14 14:15:14 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,27 +1,9 @@
 import math
 
-PI = 3.14159265
-
-print("enter a")
+print("a = ")
 a = float(input())
-print("enter b")
-b = float(input())
-print("enter c")
-c = float(input())
 
-alpha = math.acos((c * c + b * b - a * a)/(2 * b * c))
-beta = math.acos((c * c + a * a - b * b)/(2 * a * c))
-gamma = math.acos((b * b + a * a - c * c)/(2 * a * b))
-
-print("in degrees: ")
-
-print((alpha * 180) / PI)
-print((beta * 180) / PI)
-print((gamma * 180) / PI)
-
-print("in radians: ")
-
-print(alpha)
-print(beta)
-print(gamma)
+print("d = ", a * math.sqrt(2))
+print("P = ", a * 4)
+print("S = ", a * a)
 
