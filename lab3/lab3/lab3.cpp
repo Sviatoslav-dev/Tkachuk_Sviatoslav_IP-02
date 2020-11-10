abd63473355d36d4ee8364c056beb937bfcf4d7a
Sviatoslav-dev <slaviktkachuk11@gmail.com>
2020-11-10 16:16:55 +0200

    Create lab3.cpp

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Russian");
+
+    float a, eps, f, f0;
+    int n = 1;
+
+    cout << "a = ";
+    cin >> a;
+
+    if (a >= 0 && a <= 2) {
+        eps = 0.000001;
+
+        f = pow((a - 1), n) / n;
+
+        do {
+            f0 = f;
+            n++;
+
+            if (n % 2 == 1) {
+                f = f0 + pow((a - 1), n) / n;
+            }
+            else {
+                f = f0 - pow((a - 1), n) / n;
+            }
+
+        } while (f - f0 < eps);
+
+        cout << "Ln(a) = " << f << endl;
+        cout << "значення, отримане стандартною функцiєю " << log(a);
+
+    }
+}
